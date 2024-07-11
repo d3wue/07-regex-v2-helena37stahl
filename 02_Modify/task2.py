8fcd69404fa260bf30d1e6bd5f2beed3b97cf6ce
helena37stahl <168066000+helena37stahl@users.noreply.github.com>
2024-07-11 07:43:46 +0000

    fertig

--- 02_Modify/task2.py
+++ 02_Modify/task2.py
@@ -1,24 +1,61 @@
 import re
 
-reg = re.compile("([A-Z][a-z]+) ([A-Z]*) ([A-Z][a-z]+)")
 
+reg = re.compile("(Prof\. |Dr\. )*([A-Z][a-z]+) ([A-Z\.a-z]*) ?([A-Z][a-z]+)(, (PhD|MSC|Duke of Manchester|KG|KT|PC|ADC))*")
+
+#Slash um echten punkt zu escapen
+#? das z.B. Leerezichen optional ist
+#* steht für beliebig oft
+#| oder
+#^damit muss der string anfangen
+#$ damit muss der String aufhören
 m = reg.match("Uwe Meier")
-print(m)
+if m:
+    print(m.group(0))
+else:
+    print(m)
 m = reg.match("Prof. Dr. Chris C Schmidt")
-print(m)
+if m:
+    print(m.group(0))
+else:
+    print(m)
 m = reg.match("Hanna J Gruber, PhD")
-print(m)
+if m:
+    print(m.group(0))
+else:
+    print(m)
 m = reg.match("Hanna J. Gruber, PhD")
-print(m)
+if m:
+    print(m.group(0))
+else:
+    print(m)
 m = reg.match("Hanna Jana Gruber, PhD, MSc")
-print(m)
+if m:
+    print(m.group(0))
+else:
+    print(m)
 m = reg.match("Dr. Uwe Meier, MSc")
-print(m)
+if m:
+    print(m.group(0))
+else:
+    print(m)
 m = reg.match("Dr. Alfred Nobel, PhD, PhD, PhD, MSc")
-print(m)
+if m:
+    print(m.group(0))
+else:
+    print(m)
 m = reg.match("Dr. Dr. Dr. Theodore Hesburgh, PhD, PhD, PhD, MSc")
-print(m)
+if m:
+    print(m.group(0))
+else:
+    print(m)
 m = reg.match("Albert KD Klein, PhD, PhD, PhD, MSc")
-print(m)
+if m:
+    print(m.group(0))
+else:
+    print(m)
 m = reg.match("Prince William, Duke of Manchester, KG, KT, PC, ADC")
-print(m)
+if m:
+    print(m.group(0))
+else:
+    print(m)
--- 04_Make/task1.py
+++ 04_Make/task1.py
@@ -8,3 +8,39 @@ for ip in fileHandler:
 fileHandler.close()
 
 # Your code starts here
+
+
+
+#check valid IPv6 Adresses
+regv6 = re.compile('^([0-9a-f]{4}:){7}([0-9a-f]{4})$')
+
+#check valid IPv4 Adresses
+regv4 = re.compile('^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){3}((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2}))$')
+
+
+ipv6Counter = 0
+ipv4Counter = 0
+invalidCounter = 0
+
+for ip in ipAddresses:
+    m = regv6.match(ip)
+    m2 = regv4.match(ip)
+    if m != None:
+        ipv6Counter += 1
+    elif m2 != None:
+        ipv4Counter += 1
+    else:
+        invalidCounter += 1
+
+print(f'Valid IPv6 Adresses: {ipv6Counter}')
+print(f'Valid IPv4 Adresses: {ipv4Counter}')
+print(f'Invalid IP Adresses: {invalidCounter}')
+
+
+
+
+
+
+
+
+
